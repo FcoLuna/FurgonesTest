a4f39ad27c40f1c3a80fbed9ad661fdbfeaa8319
FcoLuna <fco.luna.o@gmail.com>
2015-11-19 00:22:18 -0300

    Creando tablas en active admin

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -3,5 +3,5 @@ class Parent < ActiveRecord::Base
   validates :nombre, presence:true
   validates :apellido, presence: true
   validates :pago, presence: true
-  validates :telefono, length: { in:8..8 , message: "ingrese un número correcto ejemplo 123456789"}
+  #validates :telefono, length: { in:8..8 , message: "ingrese un número correcto ejemplo 123456789"}
 end
